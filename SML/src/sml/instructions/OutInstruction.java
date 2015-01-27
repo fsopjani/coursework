4ab55171ff3ddd5ccce240da6439af37484bf584
fatos <fatos.sopjani@gmail.com>
2015-01-27 14:36:50 +0000

    adding new instructions

---
+++ SML/src/sml/instructions/OutInstruction.java
@@ -0,0 +1,34 @@
+package sml.instructions;
+
+import sml.Instruction;
+import sml.Machine;
+
+/**
+ * This class ....
+ * 
+ * @author Fatos Sopjani
+ */
+
+public class OutInstruction extends Instruction {
+
+	private int registerID;
+
+	public OutInstruction(String label, String op) {
+		super(label, op);
+	}
+
+	public OutInstruction(String label, int registerID) {
+		this(label, "out");
+		this.registerID = registerID;
+	}
+
+	@Override
+	public void execute(Machine m) {
+		m.getRegisters().getRegister(registerID);
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + " " + registerID;
+	}
+}
